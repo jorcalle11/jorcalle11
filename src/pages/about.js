7c8898656869f2357e2205cdb191e32fca7afd05
jorcalle11 <jorcalle11@gmail.com>
2019-11-17 22:36:19 -0500

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+
+const About = () => (
+  <Layout>
+    <div>This is the About page</div>
+  </Layout>
+)
+
+export default About
