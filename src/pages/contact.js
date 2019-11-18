f4945a19a0578406814b0bf276562a32eadb4c91
jorcalle11 <jorcalle11@gmail.com>
2019-11-17 22:36:31 -0500

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+
+const Contact = () => (
+  <Layout>
+    <div>This is the contact page</div>
+  </Layout>
+)
+
+export default Contact
