bc16f32f28d482a7d810107aa36c31c18dc6355f
jorcalle11 <jorcalle11@gmail.com>
2019-11-18 23:29:04 -0500

    Refactor container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,26 +2,15 @@ import React from "react"
 import styled from "styled-components"
 
 const Div = styled.div`
-  padding-right: 15px;
-  padding-left: 15px;
+  padding-right: 1rem;
+  padding-left: 1rem;
   margin-right: auto;
   margin-left: auto;
   height: 100%;
 
-  @media (min-width: 576px) {
-    max-width: 540px;
-  }
-
-  @media (min-width: 768px) {
-    max-width: 720px;
-  }
-
-  @media (min-width: 992px) {
-    max-width: 960px;
-  }
-
   @media (min-width: 1200px) {
-    max-width: 1140px;
+    padding-right: 3.5rem;
+    padding-left: 3.5rem;
   }
 `
 const Container = ({ children }) => <Div>{children}</Div>
