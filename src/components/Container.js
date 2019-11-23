29cd1cb1965eeb90f06ab2b149338a2303ab3d69
jorcalle11 <jorcalle11@gmail.com>
2019-11-23 01:00:07 -0500

    Remove media query

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,16 +2,11 @@ import React from "react"
 import styled from "styled-components"
 
 const Div = styled.div`
-  padding-right: 1rem;
-  padding-left: 1rem;
   margin-right: auto;
   margin-left: auto;
+  padding-left: 15px;
+  padding-right: 15px;
   height: 100%;
-
-  @media (min-width: 1200px) {
-    padding-right: 3.5rem;
-    padding-left: 3.5rem;
-  }
 `
 const Container = ({ children }) => <Div>{children}</Div>
 
