b07a2c7a1607fdc0464bcacdc983e5bca1c6ca09
jorcalle11 <jorcalle11@gmail.com>
2019-11-24 22:02:16 -0500

    Add bootstrap media queries

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,13 +1,32 @@
 import React from "react"
 import styled from "styled-components"
 
-const Div = styled.div`
+const ContainerFluid = styled(props => <div {...props} />)`
+  padding-right: 15px;
+  padding-left: 15px;
   margin-right: auto;
   margin-left: auto;
-  padding-left: 15px;
-  padding-right: 15px;
   height: 100%;
 `
-const Container = ({ children }) => <Div>{children}</Div>
 
-export default Container
+const Container = styled(ContainerFluid)`
+  @media (min-width: 576px) {
+    max-width: 540px;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+`
+
+export default ({ fluid = false, ...rest }) => {
+  return fluid ? <ContainerFluid {...rest} /> : <Container {...rest} />
+}
