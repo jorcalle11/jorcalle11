f68bdee86e615627cc5aa93b2ec5f14c6705e8c6
jorcalle11 <jorcalle11@gmail.com>
2020-01-12 23:31:46 -0500

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,72 +1,112 @@
 import React from "react"
 
-import Layout from "../components/Layout"
-import SEO from "../components/Seo"
-import Emoji from "../components/Emoji"
 import Container from "../components/Container"
+import Layout from "../components/Layout"
+import Link from "../components/Link"
+import SEO from "../components/Seo"
+import SocialMedia from "../components/SocialMedia"
+import Item from "../components/Item"
+import styled from "styled-components"
+
+const Div = styled.div`
+  display: flex;
+  flex-flow: column;
+
+  @media (min-width: 768px) {
+    flex-flow: row !important;
+    justify-content:space-around;
+  }
+`
+
+const H4 = styled.h4`
+  margin-top: 0;
+`
 
 const About = () => (
   <Layout>
     <SEO title="About" />
     <Container>
-      <h1>Hey, I'm Jorge</h1>
+      <h1>Hi there! 👋</h1>
       <p>
-        I'm a <b>Javascript developer</b> from{" "}
+        My name is Jorge Calle, I'm a <b>system engineer</b> and a{" "}
+        <b>javascript developer</b> with more than 4 years of experience writing
+        code in both frontend and backend side.
+      </p>
+      <p>
+        I'm currently working at{" "}
+        <a href="https://condorlabs.io/">Condor labs</a> as a senior backend
+        developer, and parallel to my work, I expend my time training and
+        learning every day about javascript and technologies such as{" "}
+        <b>Node.js</b>, <b>React</b>, <b>Firebase</b>, <b>Docker</b> and{" "}
+        <b>AWS services</b>.
+      </p>
+      <p>
+        I'm from{" "}
         <a href="https://es.wikipedia.org/wiki/Sahag%C3%BAn_(Colombia)">
-          Sahagún
+          Sahagún (CO)
         </a>
-        , Córdoba. I have been writing code for around 5 years in both front-end
-        and back-end side.
-      </p>
-      <p>
-        I'm a system enginner from the{" "}
-        <a href="https://www.unicordoba.edu.co">Universidad de Córdoba</a> and I
-        have been working at <a href="https://condorlabs.io/">Condor labs</a> as
-        a <b>Backend Developer</b> for around 3 years.
-      </p>
-      <p>
-        I like to learn new things and be informed of everything new that the
-        world of web development has to offer. I usually try to put into
-        practice what I have learned in order to improve my workflow and be more
-        productive. Also, I like to listen music and watch tv series on Netflix
-        and thanks to that, I've been able to improve my English skills.{" "}
-        <Emoji label="victory" children="✌️" />
-      </p>
-      <p>
-        When I'm not in front of a screen, I love to spend my time with my
-        little daughter, family and friends.
+        , I'm a Father, I like sports, watch TV series and football, I like to
+        play videogames, read books about technology and also learn new things.
+        When I'm not coding or I'm not in front of a screen, it is because I'm
+        playing with my little daughter, visiting my family or friends.
       </p>
       <section>
-        <h3>Skills</h3>
-        <div>
-          <p>Front-end</p>
-          <ul>
-            <li>HTML</li>
-            <li>CSS fraweworks (Bootstrap, Materialize)</li>
-            <li>Javascript</li>
-            <li>React.js</li>
-            <li>Redux</li>
-            <li>firebase</li>
-            <li>Styled components</li>
-          </ul>
-        </div>
-        <div>
-          <p>Back-end</p>
-          <ul>
-            <li>Javascript</li>
-            <li>Node.js</li>
-            <li>Express/Hapi.js</li>
-            <li>MongoDB</li>
-            <li>Redis</li>
-            <li>Serverless</li>
-            <li>AWS</li>
-          </ul>
-        </div>
+        <h2>Skills</h2>
+        <Div>
+          <div>
+            <H4>Core</H4>
+            <ul>
+              <Item>Git</Item>
+              <Item>Javascript</Item>
+              <Item>Unit tests</Item>
+              <Item>Functional programing</Item>
+            </ul>
+          </div>
+          <div>
+            <H4>Frontend</H4>
+            <ul>
+              <Item>React</Item>
+              <Item>Redux</Item>
+              <Item>Styled components</Item>
+              <Item>Webpack</Item>
+              <Item>Mobile first and Responsive Design</Item>
+            </ul>
+          </div>
+          <div>
+            <H4>Backend</H4>
+            <ul>
+              <Item>Node.js</Item>
+              <Item>Mongo DB</Item>
+              <Item>MySQL</Item>
+              <Item>Firebase</Item>
+              <Item>GraphQL</Item>
+              <Item>Redis</Item>
+              <Item>Microservices</Item>
+              <Item>Serverless</Item>
+              <Item>Docker</Item>
+            </ul>
+          </div>
+        </Div>
       </section>
       <p>
-        If you have a question about me or if you’d like to get in touch, feel
-        free to say hello through any of the social links.
+        If you came here only to find what theme, laptop or apps that I use,
+        then take a look to my{" "}
+        <Link to="/uses" style={{ color: "var(--accentColor)" }}>
+          uses
+        </Link>{" "}
+        page.
       </p>
+      <p>
+        If you have a question or you would like to get in touch, feel free to
+        say hello through any of my social medias or leave me a message in the{" "}
+        <Link to="/contact" style={{ color: "var(--accentColor)" }}>
+          contact
+        </Link>{" "}
+        page.
+      </p>
+      <section style={{ margin: "50px 0" }}>
+        <SocialMedia />
+      </section>
     </Container>
   </Layout>
 )
