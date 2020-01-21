7f9a3970dfd3b3aaa1256059f914e2354fca9abf
jorcalle11 <jorcalle11@gmail.com>
2020-01-20 22:26:23 -0500

    Add Avatar component

---
+++ src/components/Avatar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styled from "styled-components"
+
+import avatar from "../images/jorge_calle.png"
+
+export default function Avatar() {
+  return <Img src={avatar} alt="Jorge Calle" width="60" height="60" />
+}
+
+const Img = styled.img`
+  border-radius: 50%;
+  background: var(--primaryText);
+`
