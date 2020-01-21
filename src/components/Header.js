897e888cfce804dbacda516263934eab547648ad
jorcalle11 <jorcalle11@gmail.com>
2020-01-20 22:29:42 -0500

    Refactor Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,56 @@ import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
 
+import Avatar from "./Avatar"
 import Container from "./Container"
-import NavLink from "./Link"
 import DarkModeToggle from "./DarkModeToggle"
+import NavLink from "./Link"
 
-const Brand = styled.h2`
-  margin: 0;
+export default function Header({ siteTitle }) {
+  const links = ["Projects", "About", "Contact"]
 
-  a:hover {
-    text-decoration: none !important;
-  }
-`
+  const renderItem = item => (
+    <Li key={item}>
+      <NavLink
+        to={`/${item.toLowerCase()}`}
+        title={item}
+        partiallyActive={true}
+        activeStyle={activeStyle}
+      >
+        {item}
+      </NavLink>
+    </Li>
+  )
+
+  return (
+    <CustomHeader>
+      <Container>
+        <Nav>
+          <Brand to="/" title="Home">
+            <Avatar />
+            {siteTitle}
+          </Brand>
+          <DesktopMenu>
+            {links.map(renderItem)}
+            <Li>
+              <DarkModeToggle />
+            </Li>
+          </DesktopMenu>
+        </Nav>
+      </Container>
+    </CustomHeader>
+  )
+}
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: "",
+}
+
+const headerHeight = 90
 
 const CustomHeader = styled.header`
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
@@ -22,62 +61,32 @@ const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 75px;
+  height: ${headerHeight}px;
 `
 
+const Brand = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  text-decoration: none !important;
+  font-size: 1.4rem;
+  font-weight: bold;
+
+  img {
+    margin-right: 10px;
+  }
+`
+
+const DesktopMenu = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+`
 const Li = styled.li`
   list-style-type: none;
   padding: 0px 5px;
   margin: 0;
 `
-
-const Ul = styled.ul`
-  margin: 0;
-  padding: 0;
-  display: flex;
-`
-
 const activeStyle = {
   color: "var(--accentColor)",
 }
 
-const Header = ({ siteTitle }) => (
-  <CustomHeader>
-    <Container>
-      <Nav>
-        <Brand>
-          <NavLink to="/" title="Home">
-            {siteTitle}
-          </NavLink>
-        </Brand>
-        <Ul>
-          {["Projects", "About", "Contact"].map(section => (
-            <Li>
-              <NavLink
-                to={`/${section.toLowerCase()}`}
-                title={section}
-                partiallyActive={true}
-                activeStyle={activeStyle}
-              >
-                {section}
-              </NavLink>
-            </Li>
-          ))}
-          <Li>
-            <DarkModeToggle />
-          </Li>
-        </Ul>
-      </Nav>
-    </Container>
-  </CustomHeader>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
