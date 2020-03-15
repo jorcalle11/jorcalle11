c53932ac1b54b4d5e4928aa803315dd65c72b329
jorcalle11 <jorcalle11@gmail.com>
2020-03-14 20:32:28 -0500

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,112 +9,144 @@ import Link from "../components/Link"
 import SEO from "../components/Seo"
 import SocialMedia from "../components/SocialMedia"
 
-const Div = styled.div`
-  display: flex;
-  flex-flow: column;
-
-  @media (min-width: 768px) {
-    flex-flow: row !important;
-  }
-
-  div {
-    margin-right: 30px;
-  }
-`
-
-const H4 = styled.h4`
-  margin-top: 0;
-`
-
 const About = () => (
   <Layout>
     <SEO title="About" />
     <Container>
-      <h1>
-        Hi there! <Emoji label="greeting" children="👋" />
-      </h1>
-      <p>
-        My name is Jorge Calle, I'm a <b>system engineer</b> and a{" "}
-        <b>javascript developer</b> with more than 4 years of experience writing
-        code in both frontend and backend side.
-      </p>
-      <p>
-        I'm currently working at{" "}
-        <a href="https://condorlabs.io/">Condor labs</a> as a senior backend
-        developer, and parallel to my work, I expend my time training and
-        learning every day about javascript and technologies such as{" "}
-        <b>Node.js</b>, <b>React</b>, <b>Firebase</b> and <b>AWS services</b>.
-      </p>
-      <p>
-        I'm from{" "}
-        <a href="https://es.wikipedia.org/wiki/Sahag%C3%BAn_(Colombia)">
-          Sahagún (CO)
-        </a>
-        , I'm a Father, I like sports, watch TV series and football, I like to
-        play videogames, read books about technology and also, learn new things.
-      </p>
-      <p>
-        When I'm not coding or I'm not in front of a screen, it is because I'm
-        playing with my little daughter, visiting my family or friends.
-      </p>
-      <section>
-        <h2>Skills</h2>
-        <Div>
-          <div>
-            <H4>Core</H4>
-            <ul>
-              <Item>Git</Item>
-              <Item>Javascript</Item>
-              <Item>Unit tests</Item>
-              <Item>Functional programing</Item>
-            </ul>
-          </div>
-          <div>
-            <H4>Frontend</H4>
-            <ul>
-              <Item>React</Item>
-              <Item>Redux</Item>
-              <Item>Styled components</Item>
-              <Item>Mobile first and Responsive Design</Item>
-            </ul>
-          </div>
-          <div>
-            <H4>Backend</H4>
-            <ul>
-              <Item>Node.js</Item>
-              <Item>Express.js</Item>
-              <Item>Mongo DB</Item>
-              <Item>MySQL</Item>
-              <Item>Firebase</Item>
-              <Item>GraphQL</Item>
-              <Item>Redis</Item>
-              <Item>Microservices</Item>
-              <Item>Serverless</Item>
-            </ul>
-          </div>
-        </Div>
+      <section id="aboutme">
+        <h1>
+          Hi there! <Emoji label="greeting" children="👋" />
+        </h1>
+        <p>
+          My name is Jorge Calle, I'm a <b>System Engineer</b> and a{" "}
+          <b>JavaScript developer</b>. I currently work at{" "}
+          <a href="https://condorlabs.io/">Condor labs</a> as Senior Backend
+          developer. I have been there for 3 years.
+        </p>
+        <p>
+          At the same time, I dedicate part of my time to continue training and
+          learning every day about JavaScript and other technologies such as{" "}
+          <b>Node.js</b>, <b>Firebase</b>, <b>React</b> and <b>GraphQL</b>.
+        </p>
+        <p>
+          I have been writing code for 5 years and I hope to continue doing it
+          for a long time <Emoji label="smiling-face" children="😊" />. I enjoy
+          creating web applications and automating tasks that I regularly do
+          over and over again.
+        </p>
+        <p>
+          I'm from{" "}
+          <a href="https://es.wikipedia.org/wiki/Sahag%C3%BAn_(Colombia)">
+            Sahagún
+          </a>
+          , I'm a father of a 5 years old girl, I Itemke sports, I Itemke to
+          watch football and TV series, I Itemke to play videogames, I Itemke to
+          read technical books and also learn new things.
+        </p>
+        <p>
+          When I'm not coding or I'm not in front of a screen, it is because I'm
+          playing with my Itemttle daughter, visiting my family or friends.
+        </p>
       </section>
-      <p>
-        If you came here only to find what theme, laptop or apps that I use,
-        then take a look to my{" "}
-        <Link to="/uses" style={{ color: "var(--accentColor)" }}>
-          uses
-        </Link>{" "}
-        page.
-      </p>
-      <p>
-        If you have a question or you would like to get in touch, feel free to
-        say hello through any of my social medias or leave me a message in the{" "}
-        <Link to="/contact" style={{ color: "var(--accentColor)" }}>
-          contact
-        </Link>{" "}
-        page.
-      </p>
-      <section style={{ margin: "50px 0" }}>
+      <section id="skills">
+        <SectionTitle>Skills</SectionTitle>
+        <Skill
+          title="Frontend Development"
+          items={["React", "Redux", "CSS in Js", "Styled Components"]}
+        />
+        <Skill
+          title="Backend Development"
+          items={[
+            "Node.js",
+            "Serverless",
+            "Microservices",
+            "Express",
+            "MongoDB",
+            "SQL",
+            "Redis",
+            "Firebase",
+            "GraphQL",
+          ]}
+        />
+        <Skill title="Languages" items={["JavaScript"]}></Skill>
+      </section>
+      <section id="experience">
+        <SectionTitle>Experience</SectionTitle>
+        <Experience company="Condor Labs">
+          <Ul>
+            <Item>
+              Backend Developer <Period>March 2018 - Current</Period>
+            </Item>
+            <Item>
+              Frontend Developer <Period>Jul 2017 - Feb 2018</Period>
+            </Item>
+            <Item>
+              Software Enginner <Period>Jan 2017 - June 2017</Period>
+            </Item>
+          </Ul>
+        </Experience>
+        <Experience company="Domoti S.A.S">
+          <Ul>
+            <Item>
+              Frontend Developer <Period>Aug 2016 - Dec 2016</Period>
+            </Item>
+          </Ul>
+        </Experience>
+      </section>
+      <section id="social" style={{ margin: "50px 0" }}>
+        <p>
+          If you have any questions or you would like to get in touch, feel free
+          to say hello through my social networks or leave me a message on the{" "}
+          <Link to="/contact" style={{ color: "var(--accentColor)" }}>
+            contact
+          </Link>{" "}
+          page.
+        </p>
         <SocialMedia />
       </section>
     </Container>
   </Layout>
 )
+
+function Skill({ title, items = [] }) {
+  return (
+    <div>
+      <H3>{title}</H3>
+      <P>{items.join(", ")}</P>
+    </div>
+  )
+}
+
+function Experience({ company, children }) {
+  return (
+    <div>
+      <H3>{company}</H3>
+      {children}
+    </div>
+  )
+}
+
+const SectionTitle = styled.h2`
+  margin-top: 40px;
+`
+
+const H3 = styled.h3`
+  margin-bottom: 0;
+`
+
+const P = styled.p`
+  margin-top: 0;
+`
+
+const Period = styled.span`
+  color: var(--secondaryText);
+  font-size: 1rem;
+  margin-left: 25px;
+`
+
+const Ul = styled.ul`
+  margin: 0;
+  padding-left: 20px;
+`
 
 export default About
