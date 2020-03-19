23c8432cead5c77bb5b62ae78157e3bee030a29d
jorcalle11 <jorcalle11@gmail.com>
2020-03-18 21:52:17 -0500

    Add firebase functions

---
+++ firebase/functions/index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const functions = require("firebase-functions")
+const nodemailer = require("nodemailer")
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!")
+})
