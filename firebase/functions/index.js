42985059c16d251c790613e13325eb8f182cb831
jorcalle11 <jorcalle11@gmail.com>
2020-03-28 22:52:25 -0500

    Add function to send email

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -3,9 +3,45 @@
 const functions = require("firebase-functions")
 const nodemailer = require("nodemailer")
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const transporter = nodemailer.createTransport({
+  host: "mail.privateemail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: functions.config().email.user,
+    pass: functions.config().email.password,
+  },
+})
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!")
+exports.contact = functions.https.onRequest(async (request, response) => {
+  const { name, email, text } = request.body
+  const sender = `${name} - <${email}>`
+  const receipt = "jorcalle11@gmail.com"
+  const subject = `Message of ${name} from jorgecalle.co`
+  const message = {
+    from: sender,
+    to: receipt,
+    subject,
+    text,
+  }
+
+  try {
+    console.log(`>${name} Sending message...`)
+    await transporter.verify()
+    const info = await transporter.sendMail(message)
+    console.log("> Message sent: %s", info.messageId)
+
+    response.send({
+      statusCode: 200,
+      code: "Ok",
+      message: `message sent ${info.messageId}`,
+    })
+  } catch (error) {
+    console.log("> Send failure!", error.message)
+
+    response.status(500).send({
+      statusCode: 500,
+      message: error.message,
+    })
+  }
 })
