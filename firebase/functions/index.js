3a38deff1fba363d1b1acc8101631be35bd9fa8e
jorcalle11 <jorcalle11@gmail.com>
2020-04-10 14:55:19 -0500

    Fix cors error

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -8,6 +8,7 @@ const remark2rehype = require("remark-rehype")
 const doc = require("rehype-document")
 const format = require("rehype-format")
 const html = require("rehype-stringify")
+const cors = require("cors")({ origin: true })
 
 function markdownToHtml(markdownString) {
   return unified()
@@ -30,71 +31,41 @@ const transporter = nodemailer.createTransport({
   },
 })
 
-function isAvalidRequest(request) {
-  const { hostname } = request
-  const whiteList = ["jorgecalle.co", "console.cloud.google.com"]
+function handleRequest(request, response) {
+  return cors(request, response, async () => {
+    const { name, email, message } = request.body
+    const sender = `"jorgecalle.co 🤖"  <${functions.config().email.user}>`
+    const receipt = '"Jorge" <jorcalle11@gmail.com>'
+    const subject = `Message of ${name}`
+    const originalSender = `"${name}" <${email}>`
 
-  if (whiteList.includes(hostname) || hostname.includes("jorcalle11.now.sh")) {
-    return true
-  } else {
-    return false
-  }
-}
+    try {
+      const html = await markdownToHtml(message)
+      await transporter.verify()
 
-async function handleRequest(request, response) {
-  response.set("Access-Control-Allow-Origin", "*")
+      console.log(`>${name} Sending message...`)
+      const info = await transporter.sendMail({
+        from: sender,
+        to: receipt,
+        cc: originalSender,
+        subject,
+        html,
+      })
 
-  if (!isAvalidRequest(request)) {
-    response.status(403).send({
-      statusCode: 403,
-      message: "Unacceptable request",
-    })
-    return
-  }
+      console.log("> Message sent: %s", info.messageId)
+      response.send({
+        statusCode: 200,
+        message: `message sent ${info.messageId}`,
+      })
+    } catch (error) {
+      console.log("> Send failure!", error.message)
 
-  if (request.method === "OPTIONS") {
-    // Send response to OPTIONS requests
-    response.set("Access-Control-Allow-Methods", "GET,POST")
-    response.set("Access-Control-Allow-Headers", "Content-Type")
-    response.send({
-      statusCode: 200,
-      message: "CORS ok",
-    })
-    return
-  }
-
-  const { name, email, message } = request.body
-  const sender = `"jorgecalle.co 🤖"  <${functions.config().email.user}>`
-  const receipt = "jorcalle11@gmail.com"
-  const subject = `Message of ${name}`
-  const originalSender = `"${name}" <${email}>`
-
-  try {
-    const html = await markdownToHtml(message)
-    await transporter.verify()
-
-    console.log(`>${name} Sending message...`)
-    const info = await transporter.sendMail({
-      from: sender,
-      to: receipt,
-      cc: originalSender,
-      subject,
-      html,
-    })
-
-    console.log("> Message sent: %s", info.messageId)
-    response.send({
-      statusCode: 200,
-      message: `message sent ${info.messageId}`,
-    })
-  } catch (error) {
-    console.log("> Send failure!", error.message)
-
-    response.status(500).send({
-      statusCode: 500,
-      message: error.message,
-    })
-  }
+      response.status(500).send({
+        statusCode: 500,
+        message: error.message,
+      })
+    }
+  })
 }
 
 exports.contact = functions.https.onRequest(handleRequest)
