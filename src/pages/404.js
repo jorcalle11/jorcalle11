e11c750fbefa9e97fee01e62415def659a760465
jorcalle11 <jorcalle11@gmail.com>
2020-04-11 00:08:04 -0500

    Not found page

---
+++ src/pages/404.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { navigate } from "gatsby"
+
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+import Container from "../components/Container"
+
+const NotFound = () => (
+  <Layout>
+    <SEO title="Not found" />
+    <Container>
+      <h1>Page not found</h1>
+      <p>You hit a route that doesn't exist.</p>
+      <pre>{JSON.stringify(navigateTo())}</pre>
+      <p>
+        <a
+          style={{ color: "var(--accentColor)" }}
+          onClick={e => {
+            e.preventDefault()
+            navigate(-1)
+          }}
+        >
+          Go Back
+        </a>
+      </p>
+    </Container>
+  </Layout>
+)
+
+export default NotFound
