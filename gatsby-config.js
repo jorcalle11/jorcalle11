236b96e65be543d446a62ca1e3e4e15770f1c879
jorcalle11 <jorcalle11@gmail.com>
2020-04-10 23:51:24 -0500

    Update description and setup google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   siteMetadata: {
     title: `Jorge Calle`,
     siteUrl: `https://www.jorgecalle.co`,
-    description: `Hi, I'm Jorge Calle! I'm a javascript developer. Welcome to my personal site!`,
+    description: `Hi, I'm Jorge Calle! I'm a System Engineer and a JavaScript Developer from Sahagún. Welcome to my personal site!`,
     author: `@jorcalle11`,
   },
   plugins: [
@@ -18,13 +18,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Jorge Calle - Javascript Developer`,
+        name: `Jorge Calle - JavaScript Developer`,
         short_name: `Jorge Calle`,
         start_url: `/`,
         background_color: `#F44336`,
         theme_color: `#F44336`,
         display: `minimal-ui`,
         icon: `src/images/jorge_calle_150.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-161163794-1",
       },
     },
   ],
