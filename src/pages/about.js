f277ff2472bfd049129a438143e4e48902746b16
jorcalle11 <jorcalle11@gmail.com>
2020-06-21 15:51:12 -0500

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,47 +15,51 @@ const About = () => (
           Hi there! <Emoji label="greeting" children="👋" />
         </h1>
         <p>
-          My name is Jorge Calle, I'm a <b>System Engineer</b> and a{" "}
-          <b>JavaScript developer</b>. I currently work at{" "}
-          <a href="https://condorlabs.io/">Condor labs</a> as{" "}
-          <b>Senior Backend developer</b>.
+          My name is Jorge Calle. I am a <strong>System engineer</strong> and a{" "}
+          <strong>Javascript developer</strong>. I have been writing code for
+          about 4 years, I hope to keep doing it for a long time{" "}
+          <Emoji label="smiling-face" children="😊" />. I'm currently working at{" "}
+          <a href="https://condorlabs.io/">Condor Labs</a> as a{" "}
+          <strong>Software engineer</strong>.
         </p>
         <p>
           At the same time, I dedicate part of my time to continue training and
-          learning every day about JavaScript and other technologies such as{" "}
-          <b>Node.js</b>, <b>Firebase</b>, <b>React</b> and <b>GraphQL</b>.
+          learning every day about <strong>JavaScript</strong> and other
+          technologies such as <strong>Node.js</strong>,{" "}
+          <strong>Firebase</strong>, <strong>React</strong>, and{" "}
+          <strong>GraphQL</strong>. My favorite platforms to take online courses
+          are <a href="https://www.pluralsight.com">Pluralsight</a>,{" "}
+          <a href="https://www.egghead.io">Egghead</a>, and{" "}
+          <a href="https://www.udemy.com">Udemy</a>.
         </p>
         <p>
-          I have been writing code for 5 years and I hope to continue doing it
-          for a long time <Emoji label="smiling-face" children="😊" />. I enjoy
-          creating web applications and automating tasks that I regularly do
-          over and over again.
-        </p>
-        <p>
-          I'm from{" "}
+          Born and raised in{" "}
           <a href="https://es.wikipedia.org/wiki/Sahag%C3%BAn_(Colombia)">
             Sahagún
           </a>
-          , I'm a father of a 5 years old girl, I like sports, I like to watch
-          football and TV series, I like to play videogames, I like to read
-          technical books and also learn new things.
+          , I moved to Cartagena(CO) a few years ago to begin my professional
+          career at Condor Labs. But when I have the opportunity (Holidays) or
+          just want to relax, I travel back to Sahagún, because for me, it is
+          the best place where I can rest{" "}
+          <Emoji label="winking-face-with-tongue" children="😜" />.
         </p>
         <p>
-          When I'm not coding or I'm not in front of a screen, it is because I'm
-          playing with my little daughter, visiting my family or friends.
+          I am a father of a 5 years old girl. I like to play and watch soccer,
+          watch TV series, and play videogames. When I am not coding or in front
+          of a screen, it is because I am playing with my daughter, visiting my
+          family or friends.
         </p>
-      </section>
-      <section id="social" style={{ margin: "50px 0" }}>
         <p>
-          Interested in my professional profile? Click{" "}
+          Are you interested in seeing my professional profile? Click{" "}
           <Link to="/cv" style={{ color: "var(--accentColor)" }}>
             here
           </Link>{" "}
-          to see a summary of my curriculum vitae.
+          to see my resume.
         </p>
         <p>
-          If you have any questions or you would like to get in touch, feel free
-          to say hello through my social networks or leave me a message on the{" "}
+          If you have any questions or you would like to get in touch with me,
+          feel free to say hello on my social networks or leave me a message on
+          the{" "}
           <Link to="/contact" style={{ color: "var(--accentColor)" }}>
             contact
           </Link>{" "}
